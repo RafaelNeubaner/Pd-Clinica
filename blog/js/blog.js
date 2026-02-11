778f2fa7fcd253f860b0ee6b6c87b6c95ef45031
Pedro Manoel <pedroferreiramanoel@gmail.com>
2026-02-11 12:06:30 -0300

    feat: página de artigos do blog

---
+++ blog/js/blog.js
@@ -0,0 +1,52 @@
+import {artigos} from "/js/dados/artigos.js";
+
+const blogPostTemplate = document.getElementById("blogPostTemplate");
+
+function showPosts(posts){
+    const postsContainer = document.getElementById("blogPosts");
+    postsContainer.innerHTML = "";
+    for (let post of posts) {
+        let postElement = blogPostTemplate.content.cloneNode(true);
+        postElement.querySelector("img").src = post.imagem;
+        postElement.querySelector("img").alt = `Imagem do artigo ${post.titulo}`;
+        postElement.querySelector("h3").textContent = post.titulo;
+        postElement.querySelector("p").textContent = post.resumo;
+        postsContainer.appendChild(postElement);
+    }
+}
+
+
+let filterArtigos = document.getElementById("filterForm");
+let assuntoField = document.getElementById("assuntoField");
+let dataField = document.getElementById("dataField");
+let especialidadeSelect = document.getElementById("especialidadeSelect");
+let conteudoSelect = document.getElementById("conteudoSelect");
+let btnFilter = document.querySelector(".btnFilter");
+
+btnFilter.addEventListener("click", function(){
+    let filteredPosts = artigos;
+
+    if(assuntoField.value){
+        filteredPosts = filteredPosts.filter(post => post.titulo.toLowerCase().includes(assuntoField.value.toLowerCase()));
+    }
+
+    if(dataField.value){
+        filteredPosts = filteredPosts.filter(post => post.data === dataField.value);
+    }
+
+    if(especialidadeSelect.value !== "Especialidade"){
+        filteredPosts = filteredPosts.filter(post => post.especialidade === especialidadeSelect.value);
+    }
+
+    if(conteudoSelect.value !== "Conteúdo"){
+        filteredPosts = filteredPosts.filter(post => post.conteudo === conteudoSelect.value);
+    }
+
+    showPosts(filteredPosts);
+});
+
+
+showPosts(artigos);
+
+
+
