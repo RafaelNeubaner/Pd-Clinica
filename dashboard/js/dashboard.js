49dfe43d790525e053cf9cbc1f3e7ba1b887b5de
Pedro Manoel <pedroferreiramanoel@gmail.com>
2026-02-12 10:00:02 -0300

    fix: correções estrutura geral mobile

--- dashboard/js/dashboard.js
+++ dashboard/js/dashboard.js
@@ -4,5 +4,12 @@ links.forEach(link => {
     link.addEventListener("click", function() {
         links.forEach(l => l.classList.remove("active"));
         this.classList.add("active");
+        if(document.body.clientWidth < 768) {
+            document.querySelector("header").classList.add("d-none");
+        }
     });
+});
+
+document.querySelector("#showMenu").addEventListener("click", function() {
+    document.querySelector("header").classList.toggle("d-none");
 });
