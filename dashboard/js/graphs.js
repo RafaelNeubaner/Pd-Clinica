421b49c5739301cee6715fe3f713f6e8b49f2c26
Pedro Manoel <pedroferreiramanoel@gmail.com>
2026-02-20 09:39:58 -0300

    fix: espaçamentos dashboard

--- dashboard/js/graphs.js
+++ dashboard/js/graphs.js
@@ -20,7 +20,7 @@ function buildGraph(elementId, labels, data) {
             plugins: {
                 legend: { display: false },
                 datalabels: {
-                    color: '#000000',
+                    color: '#ffffff',
                     anchor: 'end', 
                     align: 'start', 
                     offset: 5,
